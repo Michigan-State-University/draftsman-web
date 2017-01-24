2e68c47b1ecc9fa65b57f0da2a52dc42d0e41438
Troy Murray <tm@msu.edu>
2017-01-24 14:05:53 -0500

    Add HTTP Compression profile commands, comment out unused code

--- js/draftsman.js
+++ js/draftsman.js
@@ -1,6 +1,7 @@
 let output;
 
 let oneconnect = "create ltm profile one-connect $VANITY_URL_oneconnect"
+let profile_http_compression = "create ltm profile http-compression $VANITY_URL_httpcompression defaults-from httpcompression"
 
 
 
@@ -14,15 +15,17 @@ function generateCode() {
       output = output + "# Create OneConnect Profile" + "\r\n";
       output = output + oneconnect + "\r\n";
   }
+  if ( $("#createHttpCompression").is(":checked") ) {
+      output = output + "# Create HTTP Compression Profile" + "\r\n";
+      output = output + profile_http_compression + "\r\n\r\n";
+  }
 
-  output = output + "\r\n" + node;
-  output = output + "\r\n" + virtual_server;
-  output = output.replace(/\$mau/gi, document.getElementById('mau').value.toLowerCase());
-  output = output.replace(/\$team/gi, document.getElementById('team_name').value.toLowerCase());
-  output = output.replace(/\$app_name/gi, document.getElementById('application_name').value.toLowerCase());
-  output = output.replace(/\$node_fqdn/gi, document.getElementById('pool_member_fqdn_1').value);
-  output = output.replace(/\$node_ip/gi, document.getElementById('pool_member_ip_1').value);
-  output = output.replace(/\$vs_ip/gi, document.getElementById('vs_ip').value);
+  // output = output.replace(/\$mau/gi, document.getElementById('mau').value.toLowerCase());
+  // output = output.replace(/\$team/gi, document.getElementById('team_name').value.toLowerCase());
+  // output = output.replace(/\$app_name/gi, document.getElementById('application_name').value.toLowerCase());
+  // output = output.replace(/\$node_fqdn/gi, document.getElementById('pool_member_fqdn_1').value);
+  // output = output.replace(/\$node_ip/gi, document.getElementById('pool_member_ip_1').value);
+  // output = output.replace(/\$vs_ip/gi, document.getElementById('vs_ip').value);
 
   output = output.replace(/\$VANITY_URL/gi, $("#vanity_url").val());
   // output = virtual_server.replace("{{vs_mau}}", document.getElementById('inputMAU').value);
