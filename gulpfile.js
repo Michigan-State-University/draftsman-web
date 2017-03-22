39a5798bb60ddb689cfb7f8f72a899d0cb3e0217
Troy Murray <tm@msu.edu>
2017-03-22 10:15:07 -0400

    Add missing semicolons

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,17 @@ var gulp = require('gulp');
 
 gulp.task('js', function(){
   return gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js'])
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task('css', function(){
   return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('font', function(){
   return gulp.src(['bower_components/bootstrap/dist/fonts/*'])
-    .pipe(gulp.dest('fonts'))
+    .pipe(gulp.dest('fonts'));
 });
 
 gulp.task('default', [ 'js', 'css', 'font' ]);
